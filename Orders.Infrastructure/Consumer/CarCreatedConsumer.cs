61a991e8bb9ee7e692413310ae3e49209f304e7d
nakopia.web <nakopia.web@gmail.com>
2026-01-22 17:41:26 +0400

    cicd test

--- Orders.Infrastructure/Consumer/CarCreatedConsumer.cs
+++ Orders.Infrastructure/Consumer/CarCreatedConsumer.cs
@@ -9,9 +9,6 @@ public class CarCreatedConsumer(IListenEventRepository<CarEventModel> repository
     public async Task Consume(ConsumeContext<CarEventModel> context)
     {
         var message = context.Message;
-        
         await repository.InsertOrUpdate(message);
-        
-        Console.WriteLine($"[Orders Service] Synchronized: {message.Brand} {message.Model}");
     }
 }
